6f451163ce0567b20240aacdd33545d66b917b0a
SJRXD <rohr.olasz@gmail.com>
2022-11-16 19:08:21 -0800

    added notes to NoteLayout

--- src/NoteLayout.tsx
+++ src/NoteLayout.tsx
@@ -6,8 +6,8 @@ type NoteLayoutProps = {
 };
 
 export function NoteLayout({notes}: NoteLayoutProps) {
-    const { id} = useParams()
-    const note = notes.find(note => note.id === id)
+    const {id} = useParams();
+    const note = notes.find(note => note.id === id);
 
     if (note == null) return <Navigate to = "/" replace />
 
@@ -17,3 +17,15 @@ export function NoteLayout({notes}: NoteLayoutProps) {
 export function useNote() {
     return useOutletContext<Note>();
 };
+
+// Notes 📝
+// useParams is a hook that returns parameters from the current route and returns an object
+
+// find is an array method that returns the first element in the array that satisfies the provided testing function
+
+// replace is a boolean that indicates whether the current entry should be replaced in the history stack
+// <Navigate to = "/" replace /> is a component that navigates to the root route and replaces the current entry in the history stack
+
+// Outlet is a component that renders the child route of the current route
+// useOutletContext is a hook that returns the context of the current route
+// context is a prop that can be passed to the Outlet component
